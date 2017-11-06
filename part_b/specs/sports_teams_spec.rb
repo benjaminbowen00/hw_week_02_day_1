341039c16f847138bccfe88c0f3735d140e58c28
benjaminbowen00 <benjibowen@hotmail.com>
2017-11-06 23:13:44 +0000

    extra test for draw on points method

--- part_b/specs/sports_teams_spec.rb
+++ part_b/specs/sports_teams_spec.rb
@@ -1,3 +1,7 @@
+#better to do all tests in one go?
+#only writer if setting it as a value - not updating?
+# can you just define an @variable (e.g. a hash) and use it in functions
+# is it ok to define a variable for th first part of assert_true (e.g. when big hash)
 require('minitest/autorun')
 require('minitest/rg')
 require_relative('../sports_teams.rb')
@@ -19,6 +23,35 @@ class TestSportsTeams < MiniTest::Test
     assert_equal("Steve McCoach", @team1.coach())
   end
 
+  def test_set_coach
+    @team1.coach = "Fabio Calcio"
+    assert_equal("Fabio Calcio", @team1.coach)
+  end
+
+  def test_add_player
+    @team1.add_player("James Tackler")
+    assert_equal(["John Kicker", "Luke Scorer", "Matthew Saver", "James Tackler"], @team1.players())
+  end
+
+  def test_check_for_player
+    assert_equal(true, @team1.check_for_player("John Kicker"))
+    assert_equal(false, @team1.check_for_player("Dave Jumper"))
+  end
+
+  def test_result_points__win
+    @team1.result("win")
+    assert_equal(3, @team1.points)
+  end
+
+  def test_result_points__lose
+    @team1.result("lose")
+    assert_equal(0, @team1.points)
+  end
+
+  def test_result_points__draw
+    @team1.result("draw")
+    assert_equal(1, @team1.points)
+  end
 
 
 end
--- part_b/sports_teams.rb
+++ part_b/sports_teams.rb
@@ -1,21 +1,60 @@
 class SportsTeams
+  attr_reader :team_name, :players, :points
 
-def initialize(input_team_name, input_players, input_coach)
-  @team_name = input_team_name
-  @players = input_players
-  @coach = input_coach
-end
+  attr_accessor :coach
 
-def team_name
-  return @team_name
-end
+  def initialize(input_team_name, input_players, input_coach)
+    @team_name = input_team_name
+    @players = input_players
+    @coach = input_coach
+    @points = 0
+  end
 
-def players
-  return @players
-end
+  def add_player(player)
+    @players << player
+  end
 
-def coach
-  return @coach
-end
+  # def check_for_player(player)
+  #   for person in @players
+  #     if person == player
+  #       return true
+  #     else
+  #       return false
+  #     end
+  #   end
+  # end
+
+#refactored:
+  def check_for_player(player)
+    @players.index(player) != nil
+  end
+
+
+
+  def result(outcome)
+    result_hash = { "win"=> 3, "draw"=> 1, "lose"=>0}
+    @points += result_hash[outcome]
+  end
+
+  ##first attempt
+  # def result(outcome)
+  #   @points += 3 if outcome == "win"
+  # end
+
+  # def team_name
+  #   return @team_name
+  # end
+  #
+  # def players
+  #   return @players
+  # end
+  #
+  # def coach
+  #   return @coach
+  # end
+  #
+  # def set_coach(name)
+  #   @coach = name
+  # end
 
 end
